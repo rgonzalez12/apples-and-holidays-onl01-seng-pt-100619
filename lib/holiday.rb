dd3355fd72cc2b31054790fe43502b791924272f
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-22 07:22:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,12 +29,12 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
-    holiday.each do |holiday, supplies|
-    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+ holiday_hash.each do |season, holiday|
+  puts "#{season.capitalize}:"
+  holiday.each do |holiday, supplies|
+  puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
   end
-end
+ end
 end
 
 def all_holidays_with_bbq(holiday_hash)
