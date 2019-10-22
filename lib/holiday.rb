3a0665484acb75fbbcea731d2d3367eaa6fb9e4a
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-22 07:16:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
- array = holiday_hash[:winter].collect
+ array = holiday_hash[:winter].collect do |holiday, supply|
+   
 end
 
 def all_supplies_in_holidays(holiday_hash)
